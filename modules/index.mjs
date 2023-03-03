63cd19bfb9f50d49845a0fdf62eb2210f97c02a8
IT012312 <it012312@MacBook-Air-2.local>
2023-03-03 18:48:16 +0530

    math file commited

---
+++ modules/index.mjs
@@ -0,0 +1,15 @@
+
+import {add} from './math.mjs';
+console.log(add(1,3));
+
+import {sub} from './math.mjs';
+console.log(sub(1,3));
+
+// const {math} = require ('./math');
+
+// console.log(math.add(1,3));
+
+// (function add(a,b) 
+// {
+
+// })(1,3)
