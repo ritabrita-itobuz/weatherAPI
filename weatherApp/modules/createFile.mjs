c09a090aa3788f69ee1ee15c2893d48cb57e5ad7
IT012312 <it012312@MacBook-Air-2.local>
2023-03-10 13:06:05 +0530

    CRUD DONE 1.0

--- weatherApp/modules/createFile.mjs
+++ weatherApp/modules/createFile.mjs
@@ -1,11 +1,63 @@
 // var fs = require ('fs');
 import * as fs from 'fs';
 
-var data = fs.readFileSync('newfile.txt');
-console.log(data.toString());
+// import * as data from './weatherDB.js';
 
+// fs.writeFileSync('newfile.txt',JSON.stringify(data),(err)=>{
+//   if(err) throw err;
+// }) //first time writing to the file then commenting 
+
+let object="";
+try {
+  var weatherdata = fs.readFileSync('newfile.txt');
+  object = JSON.parse(weatherdata);
+}
+catch(err){
+  console.log(err);
+}
+// console.log (object); // for reading object
 console.log('after modification');
-fs.writeFile('newfile.txt', 'Learn Node FS module', function (err) {
+
+//-----------------(adding data)----------------
+// function addData() {
+//   object.push(
+//       {
+//           location: 'keshtopur',
+//           tempC: '13',
+//           tempF: '55.4',
+//           condition: {
+//               text:"Partly Cloudy",
+//               feelsLikeC: "12",
+//               feelsLikeF:"53.6",
+//               humidity: "30"
+//           },
+//       },
+//   )
+// }
+// addData();
+
+//-----------------(updating data)--------------
+function modifyData() {
+  const data = object.find(info => info.location=== 'Tokyo' )
+  data.tempC = "32.0";
+}
+modifyData();
+
+//-----------------(deleting data)-------------
+function deleteData() {
+  const data = object.find(info => info.location=== 'Tokyo' )
+  const position = object.indexOf(data);
+  object.splice(position,1);
+}
+deleteData();
+
+console.log (object);
+//final update to the file
+  fs.writeFileSync('newfile.txt',JSON.stringify(object),function (err) {
     if (err) throw err;
-    console.log('File is created successfully.');
-  });
+    console.log('File is updated successfully.');
+  })
+
+//read file
+// var database = fs.readFileSync('newfile.txt');
+// console.log(database.toString());
