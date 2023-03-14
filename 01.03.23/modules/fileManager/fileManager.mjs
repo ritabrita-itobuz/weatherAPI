a9928a421b241107f851d07ce071eaf856a567ce
IT012312 <it012312@MacBook-Air-2.local>
2023-03-14 12:14:57 +0530

    working on deleting folder after creation

--- 01.03.23/modules/fileManager/fileManager.mjs
+++ 01.03.23/modules/fileManager/fileManager.mjs
@@ -9,17 +9,52 @@
 //   });
 
 import * as process from 'process';
+import { createInterface } from 'readline';
+import * as fs from 'fs';
+// import * as promptSync from 'prompt-sync';
 console.log(process.argv);
 
 console.log('Press 1 for creating folder');
-function testNum(a) {
-    let result;
-    if (a > 0) {
-      result = 'positive';
-    } else {
-      result = 'NOT positive';
-    }
-    return result;
+console.log('Press 2 for deleting folder')
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+readline.setPrompt(`Enter any number `);
+readline.prompt();
+readline.on('line', (number) => {
+  if(number == 1)
+  {
+    readline.question('Enter your folder name', foldername => {
+      try {
+        if (!fs.existsSync(foldername)) {
+          fs.mkdirSync(foldername);
+          console.log(`your folder named ${foldername} just created!`);
+        }
+        else 
+          console.log(`your folder named ${foldername} already exists!`);
+      } catch (err) {
+        console.error(err);
+      }
+      readline.close();
+    });
   }
-  
-  console.log(testNum(-5));
+    
+  if(number == 2)
+  {
+    // console.log(`Are youy sure to delete this folder`);
+    readline.question(`Are you sure to delete any folder `, decision => {
+      if(decision == 'yes')
+      {
+        console.log(`okay`);
+      }
+      else {
+        console.log(`okay, Fine`);
+      }
+      readline.close();
+    });
+  }
+    
+  // readline.close();//number rl
+});
+
