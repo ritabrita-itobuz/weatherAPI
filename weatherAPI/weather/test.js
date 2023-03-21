852851f57fbbb298f43f5b3c598c419ab67699e4
IT012312 <it012312@MacBook-Air-2.local>
2023-03-21 19:01:37 +0530

    foldername changed

--- weatherAPI/weather/test.js
+++ weatherAPI/weather/test.js
@@ -1,10 +1,10 @@
 // export * from "./weatherDB.js";
 
-import * as ritu from "./weatherDB.js";
+import * as data from "./weatherDB.js";
 // console.log(ritu.weatherdata);
 
 function addData() {
-    ritu.weatherdata.push(
+    data.weatherdata.push(
         {
             location: 'keshtopur',
             tempC: '13',
@@ -22,17 +22,17 @@ addData();
 // console.log(ritu.weatherdata);
 
 function modifyData() {
-    const data = ritu.weatherdata.find(info => info.location=== 'Tokyo' )
+    const data = data.weatherdata.find(info => info.location=== 'Tokyo' )
     data.tempC = "32.0";
 }
 modifyData();
 // console.log(ritu.weatherdata);
 
 function deleteData() {
-    const data = ritu.weatherdata.find(info => info.location=== 'Tokyo' )
-    const position = ritu.weatherdata.indexOf(data);
-    ritu.weatherdata.splice(position,1);
+    const data = data.weatherdata.find(info => info.location=== 'Tokyo' )
+    const position = data.weatherdata.indexOf(data);
+    data.weatherdata.splice(position,1);
 }
 deleteData();
-console.log(ritu.weatherdata);
+console.log(data.weatherdata);
 
