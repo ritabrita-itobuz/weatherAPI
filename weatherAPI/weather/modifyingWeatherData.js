b7de36df0fc55e9e1cf26a4eb4bc789a005da593
IT012312 <it012312@MacBook-Air-2.local>
2023-03-22 10:27:56 +0530

    change made

---
+++ weatherAPI/weather/modifyingWeatherData.js
@@ -0,0 +1,38 @@
+// export * from "./weatherDB.js";
+
+import * as information from "./weatherDB.js";
+// console.log(data.weatherdata);
+
+function addData() {
+    information.weatherdata.push(
+        {
+            location: 'Bardhaman',
+            tempC: '13',
+            tempF: '55.4',
+            condition: {
+                text:"Partly Cloudy",
+                feelsLikeC: "12",
+                feelsLikeF:"53.6",
+                humidity: "30"
+            },
+        },
+    )
+}
+addData();
+// console.log(information.weatherdata);
+
+function modifyData() {
+    const data = information.weatherdata.find(info => info.location=== 'Tokyo' )
+    data.tempC = "33.5.0";
+}
+modifyData();
+console.log(information.weatherdata);
+
+function deleteData() {
+    const data = information.weatherdata.find(info => info.location=== 'Tokyo' )
+    const position = information.weatherdata.indexOf(data);
+    information.weatherdata.splice(position,1);
+}
+deleteData();
+// console.log(information.weatherdata);
+
